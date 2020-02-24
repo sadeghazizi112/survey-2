65794e2cd908acb445407b412c1c59d1bafab999
sadegh azizi <sadeghazizi112@yahoo.com>
2020-02-24 15:04:33 -0800

    should be done?

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 
-var name = $("yourname").val();
-var beverage = $("#beverage").val();
-var food = $("food").val();
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var name = $("#yourname").val();
+    console.log(name);
+    var beverage = $("#beverage").val();
+    console.log(beverage);
+    var food = $("#food").val();
+    console.log(food);
+    $("#output").text(name + " " + beverage + " " + food)
+    return;
+  });
+});
